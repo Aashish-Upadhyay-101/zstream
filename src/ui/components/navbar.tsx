85a732cec6b25662be9be306550b6731d68c29dd
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-10-10 07:58:58 +0545

    custom input search field

--- src/ui/components/navbar.tsx
+++ src/ui/components/navbar.tsx
@@ -1,26 +1,46 @@
-import { RiZzzFill } from "react-icons/ri";
+import React, { useState } from "react";
 import { IconInput } from "zstream/ui/primitives/input";
+import { RiZzzFill } from "react-icons/ri";
 import { BiSearch } from "react-icons/bi";
 
 interface NavbarProps {
   children?: JSX.Element;
 }
 
-const navbar = ({ children }: NavbarProps) => {
+export default function Navbar({ children }: NavbarProps) {
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const handleSearch = () => {};
+
+  const handleKeyDownSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      console.log("YOu pressed Enter");
+      handleSearch();
+    }
+  };
+
   return (
-    <nav className="fixed h-14 z-50 w-full border border-gray-200 bg-white shadow-sm lg:overflow-y-visible">
+    <nav className="fixed z-50 w-full border border-gray-200 bg-white shadow-sm lg:overflow-y-visible">
       <div className="mx-auto flex max-w-full lg:px-16 xl:grid xl:grid-cols-12">
-        <div className="flex items-center gap-2 flex-shrink-0 lg:static xl:col-span-2">
+        <div className="flex flex-shrink-0 items-center gap-2 lg:static xl:col-span-2">
           <RiZzzFill className="h-8 w-8" />
           <span className="text-xl font-semibold">ZStream</span>
         </div>
         <div className="flex-1 lg:px-0 xl:col-span-8">
-          <IconInput icon={<BiSearch className="w-5 h-5 text-slate-400" />} placeholder="Search"/>
+          <div className="flex items-center px-6 py-4">
+            <IconInput
+              icon={<BiSearch className="h-5 w-5 text-slate-400" />}
+              placeholder="Search"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setSearchInput(e.target.value)
+              }
+              onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) =>
+                handleKeyDownSearch(e)
+              }
+            />
+          </div>
         </div>
       </div>
     </nav>
   );
-};
-
-export default navbar;
- 
+}
--- src/ui/primitives/input.tsx
+++ src/ui/primitives/input.tsx
@@ -1,28 +1,36 @@
-import * as React from "react"
-import { cn } from "../lib/utils"
+import React, { useState } from "react";
+import { cn } from "../lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-    icon: JSX.Element;
-  }
+  icon: JSX.Element;
+}
 
 const IconInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, icon, ...props }, ref) => {
+    const [focus, setFocus] = useState<boolean>(false);
+
     return (
-      <div className="flex items-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50">
-        { icon }
+      <div
+        className={`focus: flex h-9 w-full items-center rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors ${
+          focus && "ring-2"
+        }`}
+      >
+        {icon}
         <input
           type={type}
           className={cn(
-            "px-3 py-1 text-base w-full border-none outline-none", 
-            className
+            "w-full border-none px-3 py-1 text-base outline-none",
+            className,
           )}
           ref={ref}
-          {...props} 
+          {...props}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
         />
       </div>
-    )
-  }
+    );
+  },
 );
 
 IconInput.displayName = "IconInput";
@@ -34,14 +42,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          className,
         )}
         ref={ref}
         {...props}
       />
-    )
-  }
-)
+    );
+  },
+);
 Input.displayName = "Input";
 
-export { Input, IconInput }
+export { Input, IconInput };
